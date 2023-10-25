66efb7c5f5403c44ab1a3d531a71a83ecf508f66
Thanthub Jamvitheelert <thanthub.ja@tdshop.io>
2023-10-25 13:59:53 +0700

    public

--- main.tf
+++ main.tf
@@ -120,7 +120,7 @@ resource "kubectl_manifest" "sync" {
 }
 
 locals {
-  known_hosts = "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
+  known_hosts = "github.com ..."
 }
 
 resource "tls_private_key" "github_deploy_key" {
